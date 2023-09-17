a4b589a29ee45623d24d2449a675e63c97c856e9
VolTorian <voltorian.social@gmail.com>
2023-09-17 13:17:20 -0700

    solid and rainbow mode selection functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const grid = document.querySelector(".grid");
 const reset = document.querySelector(".reset");
-reset.addEventListener("click", resetGrid)
+const modeButtons = document.querySelectorAll(".mode");
+
+let currentMode = "solid";
+
+reset.addEventListener("click", resetGrid);
+modeButtons.forEach((button) => button.addEventListener("click", changeMode));
 
 function generateGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -18,6 +23,17 @@ function generateGrid(size) {
 
 function applyHover() {
     this.classList.add("hover");
+    if (currentMode === "solid") {
+        this.style.backgroundColor = "";
+        this.style.borderColor = "";
+    }
+    else if (currentMode === "rainbow") {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        this.style.borderColor = `rgb(${red}, ${green}, ${blue})`;
+    }
 }
 
 function clearGrid() {
@@ -41,6 +57,17 @@ function resetGrid() {
     generateGrid(size);
     cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => cell.addEventListener("mouseover", applyHover));
+}
+
+function changeMode() {
+    if (this.textContent === "Solid") {
+        console.log("selected solid brush");
+        currentMode = "solid";
+    }
+    else if (this.textContent === "Rainbow") {
+        console.log("selected rainbow brush");
+        currentMode = "rainbow";
+    }
 }
 
 generateGrid(8);
